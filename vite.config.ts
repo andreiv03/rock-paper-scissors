09f8a56339e8dde3c1cafcf0c913f3b84e408e8f
Andrei Voicu <andrei.voicu110@icloud.com>
2025-03-05 23:20:18 +0200

    feat(gh-pages): readded gh-pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 
 export default defineConfig({
+	base: "/rock-paper-scissors/",
 	plugins: [react()],
 	resolve: {
 		alias: {
